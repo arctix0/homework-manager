21c4bb9509a004952e3869031681e753b505ac1e
Arc Tix <arctixx26@proton.me>
2022-08-27 20:00:53 +1000

    init with tauri and solid

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { createSignal } from "solid-js";
+import logo from "./assets/logo.svg";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+
+  return (
+    <div>
+      <header class="">
+        Homework Manager
+      </header>
+    </div>
+      
+  );
+}
+
+export default App;
